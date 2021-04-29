c359d0ba076142263818d9341c3e79160bc4d7f7
ardwang <278161009@qq.com>
2021-04-29 17:08:44 +0800

    update rnd 2021/4/29

--- cmake-build-debug/CMakeFiles/DataStructStudy.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/DataStructStudy.dir/cmake_clean.cmake
@@ -1,10 +1,11 @@
 file(REMOVE_RECURSE
   "CMakeFiles/DataStructStudy.dir/main.cpp.o"
+  "CMakeFiles/DataStructStudy.dir/test.c.o"
   "DataStructStudy"
   "DataStructStudy.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
-foreach(lang CXX)
+foreach(lang C CXX)
   include(CMakeFiles/DataStructStudy.dir/cmake_clean_${lang}.cmake OPTIONAL)
 endforeach()
--- main.cpp
+++ main.cpp
@@ -1,6 +1,53 @@
 #include <iostream>
 
+using namespace std;
+
+const int maxn = 505;
+
+int a[maxn][maxn];
+int b[maxn][maxn];
+int ans[maxn][maxn];
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    //矩阵加法
+    int n,m;
+    cin >>n >>m;
+
+    //输入a
+    for(int i=0; i<n; i++){
+        for(int j=0;j<m;j++){
+            cin>>a[i][j];
+        }
+    }
+
+    //输入b
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>b[i][j];
+        }
+    }
+
+    //相减
+    for(int i=0; i<n ;i++){
+        for(int j=0; j<m; j++){
+            ans[i][j]=a[i][j]-b[i][j];
+        }
+    }
+
+    //输出
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cout<<ans[i][j]<<' ';
+        }
+        cout<<endl;
+    }
+
     return 0;
+
+
+
+    //std::cout << "Hello, World!" << std::endl;
+
+    //return 0;
 }
